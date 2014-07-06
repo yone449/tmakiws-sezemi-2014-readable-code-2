5bfb9d57e5bdb932b5f09654b404b748382933cd
iwase <akihabara@iref.(none)>
2014-07-06 13:36:35 +0900

    edited for problem 5

--- tmakiws-sezemi-2014-readable-code-2/recipe.rb
+++ tmakiws-sezemi-2014-readable-code-2/recipe.rb
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 
-def output_recipe(recipe_file, encoding)
+def add_id(recipe, id)
+  return "#{id}: #{recipe}"
+end  
+
+def output_recipe(recipe_file, encoding="utf-8")
   recipe = File.open("recipe-data.txt", "r:#{encoding}")
-  recipe.each do |line|
+  recipe.each_with_index do |line, id|
+    line = add_id(line, id)
     print line
   end
 end
 
 filename, encoding = "recipe-data.txt", "utf-8"
-
 output_recipe(filename, encoding)
-
