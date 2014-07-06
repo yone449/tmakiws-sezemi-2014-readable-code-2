4c842150e1b5033d6d0b38d60409ffa2a2612dcf
yonezawa <ryota_yonezawa@ipc.i.u-tokyo.ac.jp>
2014-07-06 15:19:52 +0900

    ok

--- tmakiws-sezemi-2014-readable-code-2/recipe.rb
+++ tmakiws-sezemi-2014-readable-code-2/recipe.rb
@@ -1,16 +1,30 @@
 # -*- coding: utf-8 -*-
 
-def add_id(recipe, id)
-  return "#{id}: #{recipe}"
-end  
-
-def output_recipe(recipe_file, encoding="utf-8")
-  recipe = File.open("recipe-data.txt", "r:#{encoding}")
-  recipe.each_with_index do |line, id|
-    line = add_id(line, id)
-    print line
-  end
+def show_all_id(filename)
+	r_file = open(filename)
+	r_file.each_with_index do |line, id|
+		show = "#{id+1}:#{line}"
+		print show
+	end
+	r_file.close
+end
+def show_selected_id(filename,selected_id)
+	r_file = open(filename)
+	line = r_file.readlines[selected_id-1]
+	show = "#{selected_id}:#{line}"
+	print show
+	r_file.close
 end
 
-filename, encoding = "recipe-data.txt", "utf-8"
-output_recipe(filename, encoding)
+#print "ファイル名を入力してください。\n"
+filename =ARGV[0]
+#print "必要であれば、表示したいIDを入力してください。\n"
+selected_id=ARGV[1].to_i
+#print "ユーザー名を入力してください。\n"
+user_name=ARGV[2]
+print "ユーザー名:#{user_name}\n" 
+if selected_id==0
+	show_all_id(filename)
+else
+	show_selected_id(filename,selected_id)
+end
