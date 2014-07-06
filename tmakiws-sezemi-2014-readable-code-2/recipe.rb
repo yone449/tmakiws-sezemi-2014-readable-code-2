eca0ea54279dbc8f563bc6d71c4700e2916125c9
iwase <akihabara@iref.(none)>
2014-07-06 11:53:58 +0900

    first commit

--- tmakiws-sezemi-2014-readable-code-2/recipe.rb
+++ tmakiws-sezemi-2014-readable-code-2/recipe.rb
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
 
-print "オムライス"
-#File.write("output.txt", str)
+def output_recipe(recipe_file, encoding)
+  recipe = File.open("recipe-data.txt", "r:#{encoding}")
+  recipe.each do |line|
+    print line
+  end
+end
+
+filename, encoding = "recipe-data.txt", "utf-8"
+
+output_recipe(filename, encoding)
+
